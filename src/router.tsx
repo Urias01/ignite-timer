68b9c63ee0a75c69022c2b0de8a45e2100cff4b0
Urias Pereira <uriasluis64@gmail.com>
2024-02-14 11:58:45 -0300

    Header e Layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './page/home'
 import { History } from './page/history'
-import { DefaultLayout } from './layouts/defaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
